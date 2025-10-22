b2be710118bee7dbc59574bd19393803802177b9
Blake Peery <blakeepeery@gmail.com>
2025-10-22 01:42:39 -0600

    plz work

---
+++ config/servers.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+$PokemonServers = array (
+  'showdown' => 
+  array (
+    'name' => 'Smogon University',
+    'id' => 'asl',
+    'server' => 'aslpokemonbattling-up-railway-app',
+    'port' => 8000,
+  ),
+);
