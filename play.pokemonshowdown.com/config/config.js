d4a47ba35fc5035a69eae30d111bd514f3d76963
Blake Peery <45606916+blakestorm32@users.noreply.github.com>
2025-10-22 21:23:20 -0600

    Update config.js

--- play.pokemonshowdown.com/config/config.js
+++ play.pokemonshowdown.com/config/config.js
@@ -4,7 +4,7 @@ var Config = (typeof Config !== 'undefined' && Config) || {};
 /* Optional: serve assets (images/audio/etc.) from your UI host */
 Config.resourceprefix = 'aslshowdownui-production.up.railway.app';
 
-Config.testclient = True;
+Config.testclient = true;
 
 Config.bannedHosts = ['cool.jit.su', 'pokeball-nixonserver.rhcloud.com', /\.glitch\.me/];
 // Config.afd = true;
